1ec12ce9ecd64dcfbc908897a0dfa0b76ae880ac
Scott <bndrbots@gmail.com>
2026-07-14 23:30:09 -0700

    Delete legal.js

--- legal.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  document
-    .querySelectorAll("[data-year]")
-    .forEach((el) => (el.textContent = new Date().getFullYear()));
-  FaceScore.wireSupportLinks();
-  FaceScore.initConsent();
-})();
